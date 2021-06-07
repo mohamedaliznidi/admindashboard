f396fff38aa4ef6a4ee459ff7ac35fecde200a8f
mohamedaliznidi <mohamedali.znidi1@esprit.tn>
2021-06-08 01:35:55 +0200

    ok

--- src/app/pages/users/components/add-admin/add-admin.component.ts
+++ src/app/pages/users/components/add-admin/add-admin.component.ts
@@ -11,11 +11,17 @@ export class AddAdminComponent {
   @Input() type: string;
 
   public succ: boolean;
+  public err: boolean;
   constructor(private service: UsersService) {}
 
   public onSubmit(data): void {
     this.service.addUser(data, this.type).subscribe((res) => {
+      this.succ = null;
+      this.err = null;
       console.log(res);
+      if (res.success === true) {
+        this.succ = res.success;
+      } else this.err = true;
     });
   }
 }
